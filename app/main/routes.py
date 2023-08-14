f96a5c27a14b45a0262e4818f2635524dfe26d51
naveenkumar <naveenkumar@metamation.com>
2023-08-14 14:55:51 +0530

    Rendering with templates

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app.main import bp
 
 @bp.route('/')
 def index():
-    return 'This is main blueprint'
+    # return 'This is main blueprint'
+    return render_template('index.html')
+
